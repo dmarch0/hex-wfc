15fc43650a98c06ab1e401dec8ec99031ee47938
Daniil <dtrmnd@Daniils-MacBook-Pro.local>
2024-01-09 20:56:37 +0100

    tech: intial

---
+++ src/components/Editor.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { fabric } from 'fabric';
+
+const CANVAS_ID = 'editor-canvas';
+
+const fabricCanvas = ref();
+const canvasRef = ref();
+
+onMounted(() => {
+    canvasRef.value.width = window.innerWidth;
+    canvasRef.value.height = window.innerHeight;
+
+    fabricCanvas.value = new fabric.Canvas(CANVAS_ID);
+});
+</script>
+
+<template>
+    <canvas :id="CANVAS_ID" ref="canvasRef" />
+</template>
